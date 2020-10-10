0932f00fc8a1dee6540472f642b9dba160a477b7
Xiaowen2024 <xiaowen_yuan@berkeley.edu>
2020-10-10 14:09:50 -0700

    Initial Commit

---
+++ Livibility/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Livibility
+//
+//  Created by 袁笑闻 on 2020/10/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
